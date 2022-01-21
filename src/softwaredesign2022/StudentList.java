22504871661fde5308d5b2dd5a9f1388b0eb9116
john <dangjoh@sheridancollege.ca>
2022-01-21 09:52:32 -0500

    reverting array changes

--- src/softwaredesign2022/StudentList.java
+++ src/softwaredesign2022/StudentList.java
@@ -11,7 +11,7 @@ package softwaredesign2022;
 public class StudentList {
     public static void main(String[] args){
         
-        Student[] studentList = new Student[4];
+        Student[] studentList = new Student[3];
         
         studentList[0] = new Student("s1", "Ronak Sheth");
         studentList[1] = new Student("s2", "John D");
