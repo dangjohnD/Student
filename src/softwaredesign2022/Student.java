96a1e4038a16066951531dbbdd54cc9db3ee2808
dangjohnD <98172745+dangjohnD@users.noreply.github.com>
2022-01-28 09:48:39 -0500

    Added author comment

--- src/softwaredesign2022/Student.java
+++ src/softwaredesign2022/Student.java
@@ -2,6 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+/*
+AUTHOR: JOHN DANG
+DATE: JAN 28 2022
+*/
 package softwaredesign2022;
 
 /**
